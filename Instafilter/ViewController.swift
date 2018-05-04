9f638ce00dc0db883ddc22091ee52e48633ea256
Forrest Parker <forrestwparker@gmail.com>
2018-05-04 16:30:43 -0700

    Completed Section 2: Designing the interface

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -7,6 +7,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
