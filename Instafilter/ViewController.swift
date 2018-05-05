1dbeedc929ad6d8ff8ea73cd124673c15507df61
Forrest Parker <forrestwparker@gmail.com>
2018-05-04 17:48:45 -0700

    Completed Section 3: Importing a picture: UIImage

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -5,10 +5,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
+    
+    var currentImage: UIImage!
     
     @IBAction func changeFilter(_ sender: UIButton) {
     }
@@ -19,9 +21,26 @@ class ViewController: UIViewController {
     @IBAction func intensityChanged(_ sender: UISlider) {
     }
     
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        currentImage = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+
     }
 
     override func didReceiveMemoryWarning() {
